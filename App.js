07ac96e1e191dc8cbe3e236436e8d0b367b88aa3
WellisLopesJr-Tech <wellislopesjrofficial@gmail.com>
2024-06-06 22:17:34 -0300

    calculadora finalizada

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const estadoInicial = {
   limparVisor: false,
   operacao: null,
   valores: [0, 0],
-  posicaoCorrente: 0 
+  posicaoCorrente: 0
 }
 
 let estadoTemp = {
@@ -17,33 +17,64 @@ let estadoTemp = {
   limparVisor: false,
   operacao: null,
   valores: [0, 0],
-  posicaoCorrente: 0 
+  posicaoCorrente: 0
 }
 
 export default props => {
 
-  const [estadoCalculadora, setEstadoCalculadora] = useState({...estadoInicial})
+  const [estadoCalculadora, setEstadoCalculadora] = useState({ ...estadoInicial })
 
-adicionarDigito = n => {
-  if(n == '.' && estadoCalculadora.valorVisor.includes('.')){
-    return;
+  adicionarDigito = n => {
+    if (n == '.' && estadoCalculadora.valorVisor.includes('.')) {
+      return;
+    }
+
+    const limparVisor = estadoCalculadora.valorVisor === '0' || estadoCalculadora.limparVisor
+    const valorCorrente = limparVisor ? '' : estadoCalculadora.valorVisor
+    const valorVisor = valorCorrente + n
+
+    if (n !== '.') {
+      const novoValor = parseFloat(valorVisor)
+      const valores = estadoCalculadora.valores
+      valores[estadoCalculadora.posicaoCorrente] = novoValor
+    }
+
+    setEstadoCalculadora({ ...estadoCalculadora, valorVisor, limparVisor: false })
+
   }
 
-  const limparVisor = estadoCalculadora.valorVisor === '0' || estadoCalculadora.limparVisor
-  const valorCorrente = limparVisor ? '' : estadoCalculadora.valorVisor
-  const valorVisor = valorCorrente + n
+  limparMemoria = () => {
+    setEstadoCalculadora({ ...estadoInicial })
+  }
 
-  estadoTemp = {...estadoCalculadora, valorVisor, limparVisor: false}
-  setEstadoCalculadora({...estadoTemp})
-}
+  setOperacao = operacao => {
+    if (estadoCalculadora.posicaoCorrente == 0) {
+      setEstadoCalculadora({
+        ...estadoCalculadora,
+        operacao,
+        posicaoCorrente: 1,
+        limparVisor: true
+      })
+    } else {
+      const ehIgual = operacao === "="
+      const valores = [...estadoCalculadora.valores]
+      try {
+        valores[0] = eval(`${valores[0]} ${estadoCalculadora.operacao} 
+        ${valores[1]}`)
+      } catch (error) {
+        valores[0] = estadoCalculadora.valores[0]
+      }
 
-limparMemoria = () => {
-  console.warn("limpar memória")
-}
-
-setOperacao = operacao => {
-  console.warn(operacao)
-}
+      valores[1] = 0
+      setEstadoCalculadora({
+        valorVisor: valores[0],
+        operacao: ehIgual ? null : operacao,
+        posicaoCorrente: ehIgual ? 0 : 1,
+        limparVisor: !ehIgual,
+        valores
+      })
+    }
+  }
 
 
   return (
@@ -51,22 +82,22 @@ setOperacao = operacao => {
       <Visor valor={estadoCalculadora.valorVisor} />
       <View style={estilos.botao}>
         <Botao label="AC" onClick={limparMemoria} triplo />
-        <Botao label="/" onClick={setOperacao} orange/>
-        <Botao label="7" onClick={adicionarDigito}/>
-        <Botao label="8" onClick={adicionarDigito}/>
-        <Botao label="9" onClick={adicionarDigito}/>
-        <Botao label="*" onClick={setOperacao} orange/>
-        <Botao label="4" onClick={adicionarDigito}/>
-        <Botao label="5" onClick={adicionarDigito}/>
-        <Botao label="6" onClick={adicionarDigito}/>
-        <Botao label="-" onClick={setOperacao} orange/>
-        <Botao label="1" onClick={adicionarDigito}/>
-        <Botao label="2" onClick={adicionarDigito}/>
-        <Botao label="3" onClick={adicionarDigito}/>
+        <Botao label="/" onClick={setOperacao} orange />
+        <Botao label="7" onClick={adicionarDigito} />
+        <Botao label="8" onClick={adicionarDigito} />
+        <Botao label="9" onClick={adicionarDigito} />
+        <Botao label="*" onClick={setOperacao} orange />
+        <Botao label="4" onClick={adicionarDigito} />
+        <Botao label="5" onClick={adicionarDigito} />
+        <Botao label="6" onClick={adicionarDigito} />
+        <Botao label="-" onClick={setOperacao} orange />
+        <Botao label="1" onClick={adicionarDigito} />
+        <Botao label="2" onClick={adicionarDigito} />
+        <Botao label="3" onClick={adicionarDigito} />
         <Botao label="+" onClick={setOperacao} orange />
-        <Botao label="0" onClick={adicionarDigito} duplo/>
-        <Botao label="." onClick={adicionarDigito}/>
-        <Botao label="=" onClick={setOperacao} orange/>
+        <Botao label="0" onClick={adicionarDigito} duplo />
+        <Botao label="." onClick={adicionarDigito} />
+        <Botao label="=" onClick={setOperacao} orange />
       </View>
     </SafeAreaView>
   )
